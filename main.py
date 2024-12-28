ef36b127026726c5655b4486d4b76c3c609efdda
SEsnorre <stephan.poernbacher@outlook.com>
2024-12-28 20:53:32 +0100

    Add playerr charakter and movement

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    print("Starting asteroids!")
     # initializing pygame
-    (successes, failures) = pygame.init()
-    #print(f"Initialized {successes} modules with {failures} failures")
+    pygame.init()
     
     # create display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    # initialize clock and dt
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # initialize player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     # start game loop
     while 1:
-        # set screen to black bg
-        screen.fill((0,0,0))
-        
         # quit the game if x is pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # set screen to black bg
+        screen.fill("black")
         
+        player.draw(screen)
+        
+        player.update(dt)
         # update screen
         pygame.display.flip()
+        # pause game for 1/60 and pass passed time to dt
+        dt = clock.tick(60)/1000
     
-    
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    
-    
+     
 if __name__ == "__main__":
     main()
